9e5db7ba124d32891fa7a283dcbf85aabde3011d
HWMooo <58176899+HWMooo@users.noreply.github.com>
2022-04-26 12:44:14 +0100

    flask get request working

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return 'Welcome'
+    return 'Welcome to Flask!!!'
 
-if __name__== '__main__':
-    app.run(debug=True)
+@app.route('/films', methods=['GET', 'POST'])
+def films_handler():
+    if request.method == 'GET':
+        return jsonify([{"name": "Happy Gilmore", "Realeased":"19/06/1996"}, {"name": "Spiderman: No Way Home", "Realeased":"20/12/2021"}]), 200
+    elif request.method == 'POST':
+        data = request.json
+        return data.name
+
+
+
+
+if __name__ == '__main__':
+    app.run()
